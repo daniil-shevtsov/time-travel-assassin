55c942ad957651e0a237b616a108cb63b4c64051
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-09-07 16:07:18 +0400

    most basic movement

--- Game.cs
+++ Game.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Game : Node2D
 {
+	Player player;
+	Target target;
+
+	Weapon weapon;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		player = GetNode<Player>("Player");
+		target = GetNode<Target>("Target");
+		weapon = GetNode<Weapon>("Weapon");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Vector2 inputDir = Input.GetVector("left", "right", "forward", "backward");
+		player.MoveAndCollide(inputDir);
 	}
 }
